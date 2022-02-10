3f579a2aee162ad637d0d9b984bad5a1b3659f26
JStruk <strukjustin@gmail.com>
2022-02-10 13:37:04 -0500

    Project setup
    Initial project setup
    * bootstrapped with create react app
    * setup typescript
    * setup tailwindcss
    * setup react router
    *

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        '1/4': '25%',
+      }
+    },
+  },
+  plugins: [],
+}

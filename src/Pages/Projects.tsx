69e5d69fb274cfa6aa342b76e3e27368a1c28c18
JStruk <strukjustin@gmail.com>
2022-07-11 21:50:38 -0400

    implement projects page

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import ProjectDisplay from '../components/ProjectDisplay';
+import { projects } from '../assets/projects'
+import { Project } from '../types/Project';
 
-const Projects = ():JSX.Element => {
+const Projects = (): JSX.Element => {
     return (
-        <>Coming soon :)</>
+        <div className='p-4 pt-12 flex flex-col justify-center w-full'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-y-24 gap-x-4'>
+                {projects.map((project: Project) => {
+                    return <ProjectDisplay project={project} />
+                })}
+            </div>
+        </div>
     )
 }
 
